039c9b6165e544e2d6649d6a3941a5af63bd91d1
Aros_zi34@student.itstep.org <Aros_zi34@student.itstep.org>
2021-11-30 18:21:19 +0200

    Добавьте файлы проекта.

---
+++ LINQ/Entities/BankBug.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Entities
+{
+    /// <summary>
+    /// Сущность ошибки банкомата
+    /// </summary>
+
+    public class BankBug
+    {
+        public string Key { get; set; }
+        public string Summary { get; set; }
+        public string Status { get; set; }
+        public string Assignee { get; set; }
+        public string Labels { get; set; }
+        public string FixVersion { get; set; }
+        public string Reporter { get; set; }
+        public string IssueType { get; set; }
+        public string OriginalEstimate { get; set; }
+        public string Priority { get; set; }
+        public string Sprint { get; set; }
+        public string DueDate { get; set; }
+        public string Created { get; set; }
+        public string QADueDate { get; set; }
+    }
+
+    
+    
+}
---
+++ LINQ/EntitiesMaping/BankBugMapping.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper.Configuration;
+using LINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.EntitiesMaping
+{
+    /// <summary>
+    /// Соответсвие названий колонок из файла реальным названиям
+    /// </summary>
+    public sealed class BankBugMapping : ClassMap<BankBug>
+    {
+        public BankBugMapping()
+        {
+            Map(m => m.Key).Name("Key");
+            Map(m => m.Summary).Name("Summary");
+            Map(m => m.Status).Name("Status");
+            Map(m => m.Assignee).Name("Assignee");
+            Map(m => m.Labels).Name("Labels");
+            Map(m => m.FixVersion).Name("Fix Version/s");
+            Map(m => m.Reporter).Name("Reporter");
+            Map(m => m.IssueType).Name("Issue Type");
+            Map(m => m.OriginalEstimate).Name("? Original Estimate");
+            Map(m => m.Priority).Name("Priority");
+            Map(m => m.Sprint).Name("Sprint");
+            Map(m => m.DueDate).Name("Due Date");
+            Map(m => m.Created).Name("Created");
+            Map(m => m.QADueDate).Name("QA Due Date");
+        }
+    }
+}
