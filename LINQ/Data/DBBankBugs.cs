1713c500b30386bd6d27c07a5a63ccc58f67bcd0
Aros_zi34@student.itstep.org <Aros_zi34@student.itstep.org>
2021-11-30 18:51:19 +0200

    30.11

---
+++ LINQ/Data/DBBankBugs.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using LINQ.Entities;
+using LINQ.EntitiesMaping;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Data
+{
+    class DBBankBugs
+    {
+        string FileName = "bugs-2002.csv";
+        private List<BankBug> bugs = new List<BankBug>();
+
+        /// <summary>
+        /// получение коллекции из файла csv
+        /// </summary>
+        public void LoadFromFile()
+        {
+            //открыть файл
+            using (var reader = new StreamReader(FileName))
+
+            //передача файла системе чтения
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                //регистрация правил чтения
+                csv.Context.RegisterClassMap<BankBugMapping>();
+               //чтение коллекции
+                bugs.AddRange(csv.GetRecords<BankBug>());
+            }
+        }
+    }
+}
